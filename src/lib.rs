8eb87dbef35c498b13cec5a602644c4a34b7e654
kidkool850@gmail.com <kidkool850@gmail.com>
2026-05-07 21:19:18 -0500

    add readme to docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(nonstandard_style)]
+#![doc = include_str!("../README.md")]
 
 #[cfg(target_os = "vita")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-
 
 
